01e8a69727b8261993d0156253ecddc6e8b0baeb
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2018-01-10 14:23:29 +0100

    Refactor REPL entities names

--- api-2/src/kotlin/script/replCompilation.kt
+++ api-2/src/kotlin/script/replCompilation.kt
@@ -2,24 +2,24 @@ package kotlin.script
 
 interface ScriptReplCompiler<CC: ScriptCompileConfiguration> {
 
-    fun compile(mutableState: IReplStageState<*>,
+    fun compile(mutableState: ReplStageState<*>,
                 replStatement: ReplStatement,
                 configurator: ScriptConfigurator<CC>): ResultWithDiagnostics<CompiledReplStatement<*>>
 }
 
 interface ScriptReplInteractiveCompiler<CC: ScriptCompileConfiguration> : ScriptReplCompiler<CC> {
 
-    fun checkIsComplete(mutableState: IReplStageState<*>,
+    fun checkIsComplete(mutableState: ReplStageState<*>,
                         replStatement: ReplStatement,
                         configurator: ScriptConfigurator<CC>): ResultWithDiagnostics<Boolean>
 
-    fun suggestAutocompletion(mutableState: IReplStageState<*>,
+    fun suggestAutocompletion(mutableState: ReplStageState<*>,
                               replStatement: ReplStatement,
                               atLocation: ScriptSource.Location,
                               configurator: ScriptConfigurator<CC>): ResultWithDiagnostics<List<String>>
 }
 
-interface CompiledReplStatement<ScriptBase: Any> {
+interface CompiledReplStatement<out ScriptBase: Any> {
 
     fun instantiate(scriptEvaluationEnvironment: ScriptEvaluationEnvironment): ResultWithDiagnostics<ScriptBase>
 }
--- api-2/src/kotlin/script/replData.kt
+++ api-2/src/kotlin/script/replData.kt
@@ -9,35 +9,35 @@ const val REPL_CODE_LINE_FIRST_GEN = 1
 
 data class ReplStatement(val no: Int, val generation: Int, val code: ScriptSource)
 
-interface ILineId : Comparable<ILineId> {
+interface ReplStatementId : Comparable<ReplStatementId> {
     val no: Int
     val generation: Int
 }
 
-data class ReplHistoryRecord<out T> (val id: ILineId, val item: T)
+data class ReplHistoryRecord<out T> (val id: ReplStatementId, val item: T)
 
-interface IReplStageHistory<T> : List<ReplHistoryRecord<T>> {
+interface ReplStageHistory<T> : List<ReplHistoryRecord<T>> {
 
     fun peek(): ReplHistoryRecord<T>? = lock.read { lastOrNull() }
 
-    fun push(id: ILineId, item: T)
+    fun push(id: ReplStatementId, item: T)
 
     fun pop(): ReplHistoryRecord<T>?
 
-    fun verifiedPop(id: ILineId): ReplHistoryRecord<T>? = lock.write {
+    fun verifiedPop(id: ReplStatementId): ReplHistoryRecord<T>? = lock.write {
         if (lastOrNull()?.id == id) pop()
         else null
     }
 
-    fun reset(): Iterable<ILineId>
+    fun reset(): Iterable<ReplStatementId>
 
-    fun resetTo(id: ILineId): Iterable<ILineId>
+    fun resetTo(id: ReplStatementId): Iterable<ReplStatementId>
 
     val lock: ReentrantReadWriteLock
 }
 
-interface IReplStageState<T> {
-    val history: IReplStageHistory<T>
+interface ReplStageState<T> {
+    val history: ReplStageHistory<T>
 
     val lock: ReentrantReadWriteLock
 
@@ -45,7 +45,7 @@ interface IReplStageState<T> {
 
     fun getNextLineNo(): Int = history.peek()?.id?.no?.let { it + 1 } ?: REPL_CODE_LINE_FIRST_NO // TODO: it should be more robust downstream (e.g. use atomic)
 
-    fun <StateT : IReplStageState<*>> asState(target: Class<out StateT>): StateT =
+    fun <StateT : ReplStageState<*>> asState(target: Class<out StateT>): StateT =
             if (target.isAssignableFrom(this::class.java)) this as StateT
             else throw IllegalArgumentException("$this is not an expected instance of IReplStageState")
 }
